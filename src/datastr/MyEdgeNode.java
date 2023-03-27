8dc6d50ccc8a48c5e838ca652554828fb28b0b8f
NiklavsBergs <bergs.niklavs@gmail.com>
2023-03-27 11:04:25 +0300

    added MyEdgeNode

--- src/datastr/MyEdgeNode.java
+++ src/datastr/MyEdgeNode.java
@@ -1,5 +1,42 @@
 package datastr;
 
 public class MyEdgeNode {
+	private int indexOfVertice;
+	private float weight;
+	private MyEdgeNode next = null;
+	
+	public int getIndexOfVertice() {
+		return indexOfVertice;
+	}
+	
+	public void setIndexOfVertice(int indexOfVertice) {
+		if(indexOfVertice >= 0) {
+			this.indexOfVertice = indexOfVertice;
+		}
+		else {
+			this.indexOfVertice = -1;
+		}
+	}
+	
+	public float getWeight() {
+		return weight;
+	}
+	
+	public void setWeight(float weight) {
+		if(weight > 0 && weight < 43000) {
+			this.weight = weight;
+		}
+		else {
+			weight = 0;
+		}
+	}
+	
+	public MyEdgeNode getNext() {
+		return next;
+	}
+	
+	public void setNext(MyEdgeNode next) {
+		this.next = next;
+	}
 
 }
