51d3db60ce87155ef1ee132e06efe34c38acce03
NiklavsBergs <bergs.niklavs@gmail.com>
2023-03-27 11:21:17 +0300

    added MyVerticeNode

---
+++ src/datastr/MyVerticeNode.java
@@ -0,0 +1,29 @@
+package datastr;
+
+public class MyVerticeNode<T> {
+	private T element;
+	private MyEdgeNode firstEdge = null;
+	
+	public MyVerticeNode(T element){
+		setElement(element);
+	}
+	
+	public T getElement() {
+		return element;
+	}
+	public void setElement(T element) {
+		if(element != null) {
+			this.element = element;
+		}
+	}
+	public MyEdgeNode getFirstEdge() {
+		return firstEdge;
+	}
+	public void setFirstEdge(MyEdgeNode firstEdge) {
+		this.firstEdge = firstEdge;
+	}
+	
+	public String toString() {
+		return "" + element;
+	}
+}
